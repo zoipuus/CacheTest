59baf0d7ca8b58a09af5e3a65cf45d906944ec21
zoipuus <813271024@qq.com>
2016-02-25 11:35:52 +0800

    update WebView code

--- app/src/main/java/com/zoipuus/cachetest/web/WebActivity.java
+++ app/src/main/java/com/zoipuus/cachetest/web/WebActivity.java
@@ -1,9 +1,7 @@
 package com.zoipuus.cachetest.web;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.KeyEvent;
@@ -11,12 +9,16 @@ import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
 
+import com.zoipuus.cachetest.LogUtils;
 import com.zoipuus.cachetest.R;
 
-public class WebActivity extends Activity {
+public class WebActivity extends Activity implements View.OnClickListener {
 
     private WebView webView;
+    private EditText url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,30 +38,30 @@ public class WebActivity extends Activity {
     }
 
     private void setupViews() {
+        findViewById(R.id.search).setOnClickListener(this);
+        url = (EditText) findViewById(R.id.url);
         webView = (WebView) findViewById(R.id.web);
-        webView.loadUrl("www.szbike.com");
         WebSettings webSetting = webView.getSettings();
         //setting cache mode
-        webSetting.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
         webSetting.setAppCacheEnabled(true);
-        String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/gefdemoweb";
-        webSetting.setAppCachePath(path);
+        webSetting.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+//        String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/gefdemoweb";
+//        webSetting.setAppCachePath(path);
 
         //support JavaScript
         webSetting.setJavaScriptEnabled(true);
+        webView.loadUrl("http://www.szbike.com");
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                webView.loadUrl(url);
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+        });
         webView.setWebChromeClient(new WebChromeClient() {
             @Override
-            public void onReceivedTitle(WebView view, String title) {
-                super.onReceivedTitle(view, title);
-            }
-
-            @Override
-            public void onReceivedIcon(WebView view, Bitmap icon) {
-                super.onReceivedIcon(view, icon);
-            }
-
-            @Override
             public void onProgressChanged(WebView view, int newProgress) {
+                LogUtils.d("progress->" + newProgress);
                 super.onProgressChanged(view, newProgress);
             }
         });
@@ -72,5 +74,16 @@ public class WebActivity extends Activity {
             return true;
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            default:
+                break;
+            case R.id.search:
+                webView.loadUrl(url.getText().toString());
+                break;
+        }
     }
 }
